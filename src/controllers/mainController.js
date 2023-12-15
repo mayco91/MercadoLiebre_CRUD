db1ceeb441bdf3046f048980b8cbdad82ecec300
mayco91 <mayconitido@gmail.com>
2023-12-15 19:54:36 -0300

    correcciones

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -13,7 +13,15 @@ const controller = {
 		res.render("index",{visited,inSale,toThousand})
 	},
 	search: (req, res) => {
-		
+		const busqueda = req.body.keywords;
+		const result = [];
+		products.forEach( producto => {
+			if (producto.name.toLowerCase().includes(busqueda.toLowerCase())){
+				resultado.push(producto)
+			}
+			
+		});
+		res.render("results", {result,busqueda})
 	},
 };
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -22,7 +22,7 @@ router.put('/edit/:id', productsController.update);
 
 
 /*** DELETE ONE PRODUCT***/ 
-router.delete('delete/:id', productsController.destroy); 
+router.delete('/delete/:id', productsController.destroy); 
 
 
 module.exports = router;
