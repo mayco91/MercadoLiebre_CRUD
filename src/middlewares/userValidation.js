553828b8189c0df1f4637d92b5754f874818575c
mayco91 <mayconitido@gmail.com>
2024-01-06 21:28:28 -0300

    correcion en la vista para mostart los mensajes de errores

--- src/middlewares/userValidation.js
+++ src/middlewares/userValidation.js
@@ -3,7 +3,7 @@ const { check } = require("express-validator");
 const validateRegister = [
     check("name").notEmpty().withMessage("Debes ingresar un nombre").bail()
                 .isLength({min: 5,max: 20}).withMessage("El nombre debe contener entre 5 y 20 caracteres").bail(),
-    check("lasName").notEmpty().withMessage("Debes ingresar un apellido").bail()
+    check("lastName").notEmpty().withMessage("Debes ingresar un apellido").bail()
                 .isLength({min: 5,max: 20}).withMessage("El apellido debe contener entre 5 y 20 caracteres").bail(),
     check("email").notEmpty().withMessage("Debes ingresar un email").bail()
                 .isEmail().withMessage("Debes ingresar un email con formato valido"),
